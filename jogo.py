852ee4c0d069bf5cbd23687ebcf0e730d5de8d3e
Nathan <nathanbsena.nb@gmail.com>
2015-11-18 17:11:47 -0200

    arrumouo pulo

--- jogo.py
+++ jogo.py
@@ -6,25 +6,35 @@ sys.path.insert(0, os.getcwd() + "/mapengine")
 
 import mapengine
 from mapengine import Scene, simpleloop
-from mapengine.base import Actor, MainActor, GameObject, Event, Directions, Event
+from mapengine.base import Actor, MainActor, GameObject, Event, Directions, Event, Vector
 
 class ator(MainActor):
     weight = Directions.DOWN
+    jumping = 0
     
     # Não deixa o ator voar
     falling_counter = 0
     def move(self, direction):
-        abaixo = self.pos[0], self.pos[1] + 1
-        if direction == Directions.UP and not getattr(self.controller[abaixo], "hardness", 0) > 0:
-            return
+        abaixo = Vector((self.pos[0], self.pos[1] + 1))
+        if direction.y == -1 and not self.checa_abaixo() and self.jumping <= 0:
+           direction.y = 0 
+        
+        #if self.jumping > 0:
+        #    direction.y = -1
         return super(ator, self).move(direction)
     
-    # Faz o actor cair até o chão para começar a fase
+    def checa_abaixo(self):
+        object = self.controller[self.pos[0], self.pos[1] + self.weight[1]]
+        hardness = getattr(object, "hardness", 0)
+        return  hardness < self.strength
+
+    # Faz o actor cair até o chão 
     def update(self):
+        if self.jumping > 0:
+            self.jumping -= 1
         if (self.pos[1] < self.controller.scene.height - 2):
-            object = self.controller[self.pos[0], self.pos[1] + self.weight[1]]
-            hardness = getattr(object, "hardness", 0)
-            if hardness < self.strength and self.weight[1]:
+            
+            if self.checa_abaixo() and self.weight[1] and not self.jumping > 0:
                 self.falling_counter += 1
                 if self.falling_counter >= self.base_move_rate:
                     saved_value = self.move_counter
@@ -36,11 +46,17 @@ class ator(MainActor):
     # Método on_fire é acionado quando aperta espaço
     # Implementa o pulo
     def on_fire(self):
-        if not self.weight == Directions.DOWN:
-            return
-        self.weight = Directions.UP
-        self.move((0, -2))
-        self.events.add(Event(13, "weight", Directions.DOWN))
+        object = self.controller[self.pos[0], self.pos[1] + self.weight[1]]
+        if self.jumping == 0 and (not self.checa_abaixo() or isinstance(object, Escada)):
+            self.jumping = 20
+            self.move(Directions.UP)
+        # if not self.weight == Directions.DOWN:
+        #    return
+        #self.weight = Directions.UP
+        #self.move((0, -1))
+        # self.events.add(Event(5, "weight", Directions.DOWN))
+        
+       
 
 
 # Black corresponde à cor azul na fase
@@ -56,11 +72,12 @@ class castelo(GameObject):
 class Escada_diagonal(GameObject):
     hardness = 5
     def on_touch(self, other):
-        if isinstance(other, ator):
+        if False and isinstance(other, ator):
             if other.pos[1] > 0:
-                other.pos = other.pos[0], other.pos[1] - 1
-                other.weight = (0,0)
-                other.events.add(Event(other.base_move_rate * 2, "weight", (0,1)))
+                pass
+                # other.pos = other.pos[0], other.pos[1] - 1
+                # other.weight = (0,0)
+                # other.events.add(Event(other.base_move_rate * 2, "weight", (0,1)))
         super(Escada_diagonal, self).on_touch(other)
 
 
@@ -68,21 +85,21 @@ class Escada(GameObject):
     hardness = 2
     def on_over(self, other):
         if isinstance(other, ator):
-            other.weight = 0, 0
-            other.events.add(Event(5, "weight", (0, 1)))
+            other.weight = Vector((0, 0))
+            other.events.add(Event(5, "weight", Vector((0, 1))))
         super(Escada, self).on_touch(other)
         
         
 class Portal(GameObject):
     hardness = 10
     def on_touch(self, other):
-        cena = Scene("fase_3")
+        cena = Scene("fase_2")
         self.controller.load_scene(cena)
         self.controller.force_redraw = True
            
     
 def main():
-    scene = Scene("fase_2")
+    scene = Scene("fase_1")
     simpleloop(scene, (800,600),)
 
  
